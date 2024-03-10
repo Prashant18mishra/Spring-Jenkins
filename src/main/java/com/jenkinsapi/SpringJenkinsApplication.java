9458e78ac4b5aedd7654a8949fdab59b03219c4f
Prashant mishra <kumarprashant7066@gmail.com>
2024-03-11 02:04:41 +0530

    second commit

--- src/main/java/com/jenkinsapi/SpringJenkinsApplication.java
+++ src/main/java/com/jenkinsapi/SpringJenkinsApplication.java
@@ -1,12 +1,29 @@
 package com.jenkinsapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
+
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
+	
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started...");
+	}
+	
 
 	public static void main(String[] args) {
+		logger.info("Application Executed...");
+		logger.info("Application Executed Second time......");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
